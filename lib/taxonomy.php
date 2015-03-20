bb13aefe8b55a6c9102a92bb89691dadfe584f90
Eric Juden <ericjuden@gmail.com>
2015-03-19 23:12:33 -0500

    Customized the wp_dropdown_categories method from WordPress to allow for multiple select.

--- lib/taxonomy.php
+++ lib/taxonomy.php
@@ -26,6 +26,133 @@ class WDH_Taxonomy {
 	function create($term){
 		return wp_create_term($term, $this->taxonomy);
 	}
+
+	// Modified copy of WordPress wp_dropdown_categories method
+	function dropdown($args = ''){
+		$defaults = array(
+			'show_option_all' => '', 
+			'show_option_none' => '',
+			'orderby' => 'id', 
+			'order' => 'ASC',
+			'show_count' => 0,
+			'hide_empty' => 1, 
+			'child_of' => 0,
+			'exclude' => '', 
+			'echo' => 1,
+			'selected' => 0, 
+			'hierarchical' => 0,
+			'name' => 'cat', 
+			'id' => '',
+			'class' => 'postform', 
+			'depth' => 0,
+			'tab_index' => 0, 
+			'taxonomy' => $this->taxonomy,
+			'hide_if_empty' => false, 
+			'option_none_value' => -1,
+			'multiple_select' => false
+		);
+
+		$defaults['selected'] = ( is_category() ) ? get_query_var( 'cat' ) : 0;
+
+		// Back compat.
+		if ( isset( $args['type'] ) && 'link' == $args['type'] ) {
+			_deprecated_argument( __FUNCTION__, '3.0', '' );
+			$args['taxonomy'] = 'link_category';
+		}
+
+		$r = wp_parse_args( $args, $defaults );
+		$option_none_value = $r['option_none_value'];
+
+		if ( ! isset( $r['pad_counts'] ) && $r['show_count'] && $r['hierarchical'] ) {
+			$r['pad_counts'] = true;
+		}
+
+		$tab_index = $r['tab_index'];
+
+		$tab_index_attribute = '';
+		if ( (int) $tab_index > 0 ) {
+			$tab_index_attribute = " tabindex=\"$tab_index\"";
+		}
+		$categories = get_terms( $r['taxonomy'], $r );
+		$name = esc_attr( $r['name'] );
+		$class = esc_attr( $r['class'] );
+		$id = $r['id'] ? esc_attr( $r['id'] ) : $name;
+
+		$multiple_select = $r['multiple_select'];
+		$multiple_select_attribute = '';
+		if($multiple_select){
+			$multiple_select_attribute = " multiple=\"multiple\"";
+		}
+
+		if ( ! $r['hide_if_empty'] || ! empty( $categories ) ) {
+			$output = "<select name='$name' id='$id' class='$class'$multiple_select_attribute $tab_index_attribute>\n";
+		} else {
+			$output = '';
+		}
+		if ( empty( $categories ) && ! $r['hide_if_empty'] && ! empty( $r['show_option_none'] ) ) {
+
+			/**
+			 * Filter a taxonomy drop-down display element.
+			 *
+			 * A variety of taxonomy drop-down display elements can be modified
+			 * just prior to display via this filter. Filterable arguments include
+			 * 'show_option_none', 'show_option_all', and various forms of the
+			 * term name.
+			 *
+			 * @since 1.2.0
+			 *
+			 * @see wp_dropdown_categories()
+			 *
+			 * @param string $element Taxonomy element to list.
+			 */
+			$show_option_none = apply_filters( 'list_cats', $r['show_option_none'] );
+			$output .= "\t<option value='" . esc_attr( $option_none_value ) . "' selected='selected'>$show_option_none</option>\n";
+		}
+
+		if ( ! empty( $categories ) ) {
+
+			if ( $r['show_option_all'] ) {
+
+				/** This filter is documented in wp-includes/category-template.php */
+				$show_option_all = apply_filters( 'list_cats', $r['show_option_all'] );
+				$selected = ( '0' === strval($r['selected']) ) ? " selected='selected'" : '';
+				$output .= "\t<option value='0'$selected>$show_option_all</option>\n";
+			}
+
+			if ( $r['show_option_none'] ) {
+
+				/** This filter is documented in wp-includes/category-template.php */
+				$show_option_none = apply_filters( 'list_cats', $r['show_option_none'] );
+				$selected = selected( $option_none_value, $r['selected'], false );
+				$output .= "\t<option value='" . esc_attr( $option_none_value ) . "'$selected>$show_option_none</option>\n";
+			}
+
+			if ( $r['hierarchical'] ) {
+				$depth = $r['depth'];  // Walk the full depth.
+			} else {
+				$depth = -1; // Flat.
+			}
+			$output .= walk_category_dropdown_tree( $categories, $depth, $r );
+		}
+
+		if ( ! $r['hide_if_empty'] || ! empty( $categories ) ) {
+			$output .= "</select>\n";
+		}
+		/**
+		 * Filter the taxonomy drop-down output.
+		 *
+		 * @since 2.1.0
+		 *
+		 * @param string $output HTML output.
+		 * @param array  $r      Arguments used to build the drop-down.
+		 */
+		$output = apply_filters( 'wp_dropdown_cats', $output, $r );
+
+		if ( $r['echo'] ) {
+			echo $output;
+		}
+		return $output;
+	}
 	
 	function get($term_id){
 		return get_term($term_id, $this->taxonomy, OBJECT);
